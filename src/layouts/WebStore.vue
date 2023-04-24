8cb04b89cdc30c750f731139206f3c12a731ec7e
sudipta <sudipta@sudipta-localhost>
2023-04-24 14:43:57 +0530

    Some changes

---
+++ src/layouts/WebStore.vue
@@ -0,0 +1,56 @@
+<template>
+  <q-layout
+    view="hhh lpR fff"
+    :class="$q.dark.isActive ? 'bg-black' : 'bg-secondary'"
+  >
+    <q-header
+      elevated
+      :class="$q.dark.isActive ? 'q-header--dark' : 'q-header--light'"
+      height-hint="200"
+    >
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          WebStore
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          Title
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
+</template>
+
+<script>
+import { useQuasar } from "quasar";
+export default {
+  data() {
+    return {
+      leftDrawerOpen: true,
+    };
+  },
+  mounted() {
+    const $q = useQuasar();
+    console.log($q.dark.isActive);
+    console.log($q.dark.mode);
+  },
+};
+</script>
