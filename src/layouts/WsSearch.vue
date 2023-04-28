cbfebe8d6e777c713d105f533eb3b99b8b35037e
Sudipta Roy <sdptroy3@gmail.com>
2023-04-28 14:27:52 +0530

    Added Search bar

--- src/layouts/WsSearch.vue
+++ src/layouts/WsSearch.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="ws-search z-max self-center"
+    :class="classes"
     @click.prevent="onClick"
     @focusin="onFocusin"
     @focusout="onFocusout"
@@ -18,7 +19,8 @@
       <q-icon
         class="ws-search__icon cursor-pointer"
         size="24px"
-        name="search"
+        :name="icon.name"
+        @click="icon.onClick"
       />
     </div>
   </div>
@@ -28,23 +30,48 @@ export default {
   name: "WsSearch",
   data() {
     return {
-      hasFocus: { type: Boolean, value: false },
-      terms: { type: String, value: "" },
+      hasFocus: false,
+      terms: "",
+      focusTimer: null,
+      focusTime: 150,
     };
+  },
+  computed: {
+    icon() {
+      if (this.terms.length > 0) {
+        return {
+          name: "clear",
+          onClick: this.resetSearch,
+        };
+      } else {
+        return { name: "search", onClick: () => {} };
+      }
+    },
+    classes() {
+      return this.hasFocus ? "ws-search__focused" : null;
+    },
   },
   methods: {
     onClick() {
       this.$refs.inputRef.focus();
-      onFocusin();
+      this.onFocusin();
     },
     onFocusin() {
+      clearTimeout(this.focusTimer);
       this.hasFocus = true;
     },
     onFocusout() {
-      this.hasFocus = false;
+      let that = this;
+      clearTimeout(this.focusTimer);
+      this.focusTimer = setTimeout(() => {
+        that.hasFocus = false;
+      }, this.focusTime);
     },
     onKeydown(evt) {
       console.log(evt);
+    },
+    resetSearch() {
+      this.terms = "";
     },
   },
 };
@@ -58,6 +85,46 @@ export default {
   width: inherit;
   height: inherit;
   cursor: text;
-  transition: box-shadow 0.28s ease-in-out;
+  /*transition: box-shadow 0.28s ease-in-out;*/
+}
+.ws-search input {
+  font-size: 16px;
+  width: 1px !important;
+  border: 0;
+  outline: 0;
+  background: none;
+}
+.ws-search__focused {
+  transform: scale3d(1, 1, 1);
+}
+.ws-search__focused .ws-search__icon {
+  display: inline-block !important;
+}
+body.body--light .ws-search input {
+  color: #474747;
+}
+body.body--light .ws-search__field {
+  background: grey;
+}
+body.body--light .ws-search__focused {
+  border: 1px solid #dddddd;
+}
+body.body--light .ws-search__focused .ws-search__field {
+  background-color: rgba(0, 0, 0, 0.28);
+}
+body.body--dark .ws-search input {
+  color: #fff;
+}
+body.body--dark .ws-search__field {
+  background-color: rgba(#fff, 0.12);
+}
+body.body--dark .ws-search__icon {
+  color: #00b4ff;
+}
+body.body--dark .ws-search__focused {
+  border: 1px solid #dddddd;
+}
+body.body--dark .ws-search__focused .ws-search__field {
+  background-color: rgba(0, 0, 0, 0.28);
 }
 </style>
